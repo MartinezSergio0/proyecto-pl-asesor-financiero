938f095c3ff2cd98ff10099efc053305eb3e2db2
FabianBetancourtFernandez <al22760578@ite.edu.mx>
2025-11-28 19:02:52 -0800

    separar base de conocimiento del motor

---
+++ base_conocimiento.py
@@ -0,0 +1,13 @@
+hechos = (
+    ('gastos', 'fijos', 'renta', 'alta'),
+    ('gastos', 'fijos', 'electricidad', 'alta'),
+    ('gastos', 'fijos', 'telefono/internet', 'media'),
+    ('gastos', 'fijos', 'seguros', 'media'),
+    ('gastos', 'variables', 'comida', 'alta'),
+    ('gastos', 'variables', 'transporte', 'media'),
+    ('gastos', 'variables', 'ropa', 'baja'),
+    ('gastos', 'variables', 'entretenimiento', 'baja'),
+    ('gastos', 'variables', 'intereses deuda', 'alta'),
+    ('objetivo', .50, ('alta', 'media')),
+    ('objetivo', .30, ('baja',))
+)
--- motor_inferencia.py
+++ motor_inferencia.py
@@ -1,17 +1,5 @@
 import math
-hechos = (
-    ('gastos', 'fijos', 'renta', 'alta'),
-    ('gastos', 'fijos', 'electricidad', 'alta'),
-    ('gastos', 'fijos', 'telefono/internet', 'media'),
-    ('gastos', 'fijos', 'seguros', 'media'),
-    ('gastos', 'variables', 'comida', 'alta'),
-    ('gastos', 'variables', 'transporte', 'media'),
-    ('gastos', 'variables', 'ropa', 'baja'),
-    ('gastos', 'variables', 'entretenimiento', 'baja'),
-    ('gastos', 'variables', 'intereses deuda', 'alta'),
-    ('objetivo', .50, ('alta', 'media')),
-    ('objetivo', .30, ('baja',))
-)
+from base_conocimiento import hechos
 
 '''
 ej_datos = {
